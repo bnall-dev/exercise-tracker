68f36b9234d84039553f00e7a4aaf6a93c4cf6ea
Brandon Nall <firewalkwithb@gmail.com>
2020-02-01 11:20:34 -0500

    Update before pair work

--- src/components/CreateExercise.js
+++ src/components/CreateExercise.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function CreateExercise() {
-  return <div id="createExercise"></div>;
+  let [exercises, setExercises] = useState([]);
+
+  const handleRecordExercise = () => {};
+  const handleInputChange = () => {};
+
+  return (
+    <div id="createExercise">
+      <h2>Create Exercise</h2>
+      <form id="exerciseForm" onSubmit={handleRecordExercise}>
+        <input
+          id="exerciseType"
+          type="text"
+          value="Exercise Type"
+          onChange={handleInputChange}
+        ></input>
+        <div id="setsRepsDiv">
+          <input id="setsInput" type="number"></input>
+          <input id="repsInput" type="number"></input>
+        </div>
+        <input
+          id="recordExercise"
+          type="submit"
+          value="Record Exercise"
+        ></input>
+      </form>
+    </div>
+  );
 }
 
 export default CreateExercise;
